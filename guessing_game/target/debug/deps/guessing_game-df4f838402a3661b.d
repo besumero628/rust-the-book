ac1e0c31fba9fb4ab7f98ea6fa9055e9c329ed6e
besumero628 <besumero628@gmail.com>
2022-07-05 19:17:22 +0900

    [build] guessing_game

---
+++ guessing_game/target/debug/deps/guessing_game-df4f838402a3661b.d
@@ -0,0 +1,5 @@
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/guessing_game/target/debug/deps/guessing_game-df4f838402a3661b: src/main.rs
+
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/guessing_game/target/debug/deps/guessing_game-df4f838402a3661b.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Volumes/Extreme\ SSD/10_product/20_practice/80_Rust/01_thebook/guessing_game/target/debug/guessing_game: /Volumes/Extreme\ SSD/10_product/20_practice/80_Rust/01_thebook/guessing_game/src/main.rs
