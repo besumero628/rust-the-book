b131c883b10407cdffa645573a3ad14a393a5e59
besumero628 <besumero628@gmail.com>
2022-07-05 19:13:53 +0900

    buildについて

---
+++ hello_cargo/target/debug/deps/hello_cargo-b0264b4cda2a6fb1.d
@@ -0,0 +1,5 @@
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/hello_cargo/target/debug/deps/hello_cargo-b0264b4cda2a6fb1.rmeta: src/main.rs
+
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/hello_cargo/target/debug/deps/hello_cargo-b0264b4cda2a6fb1.d: src/main.rs
+
+src/main.rs:
