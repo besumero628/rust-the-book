5293308418ca41ca339e5acc1f2c415c990869e5
besumero628 <besumero628@gmail.com>
2022-07-05 22:05:06 +0900

    [Add] break文

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -11,20 +11,26 @@ fn main() {
 
     println!("Please input your guess.");
 
-    let mut guess = String::new();
+    loop {
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number");
-
-    println!("You guessed: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => print!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number");
+    
+        println!("You guessed: {}", guess);
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
+
 }
