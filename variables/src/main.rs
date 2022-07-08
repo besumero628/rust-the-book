d5bc1a58e69ca447c09659af525618bf0b1f99e9
besumero628 <besumero628@gmail.com>
2022-07-08 17:07:15 +0900

    [Add] 関数

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,9 +1,8 @@
 fn main() {
-    let x = 5;
-    let x = x + 1;
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {}", x);
-    }
-    println!("The value of x is: {}", x);
+    println!("Hello World!");
+    another_function();
+}
+
+fn another_function() {
+    println!("Another function!");
 }
