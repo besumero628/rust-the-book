b96360eb0dceb2e47c00dd8e8f48bda977e077d5
besumero628 <besumero628@gmail.com>
2022-07-16 23:59:23 +0900

    [Add] fibonacci guess

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,8 +1,7 @@
 use std::io;
 
 fn main() {
-    println!("Hello, world!");
-
+    println!("select number");
     let mut select_number = String::new();
     io::stdin()
         .read_line(
@@ -10,6 +9,26 @@ fn main() {
         ).expect("Error");
     let select_number: u32 = select_number.trim().parse()
         .expect("Please type a number");
+    
+    let fibo_number: u32 = fibonacci(select_number);
+    println!("select number : {}", select_number);
+    println!("fibonacci number : {}", fibo_number)
+}
 
-    println!("in : {}", select_number)
+fn fibonacci(select_number: u32) -> u32 {
+    let mut one_before_number: u32 = 1;
+    let mut two_before_number: u32 = 1;
+    let mut fibo_number: u32 = 1;
+
+    if select_number < 3 {
+        fibo_number = 1;
+    } else {
+        for _i in 3..=select_number {
+            fibo_number = one_before_number + two_before_number;
+            one_before_number = two_before_number;
+            two_before_number = fibo_number;
+        }
+    }
+
+    return fibo_number
 }
