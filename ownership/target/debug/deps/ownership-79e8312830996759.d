28f6500354e0f20d8d9609999307a2518e0146c4
besumero628 <besumero628@gmail.com>
2022-07-22 12:34:59 +0900

    inital commit ownership

---
+++ ownership/target/debug/deps/ownership-79e8312830996759.d
@@ -0,0 +1,5 @@
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/ownership/target/debug/deps/ownership-79e8312830996759.rmeta: src/main.rs
+
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/ownership/target/debug/deps/ownership-79e8312830996759.d: src/main.rs
+
+src/main.rs:
---
+++ ownership/target/debug/deps/ownership-aa0a9484c193916a.d
@@ -0,0 +1,5 @@
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/ownership/target/debug/deps/ownership-aa0a9484c193916a.rmeta: src/main.rs
+
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/ownership/target/debug/deps/ownership-aa0a9484c193916a.d: src/main.rs
+
+src/main.rs:
