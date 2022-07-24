54402ac22d116357140344fc5738a8a4b75f5cda
besumero628 <besumero628@gmail.com>
2022-07-24 21:34:41 +0900

    clone

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,7 +1,6 @@
 fn main() {
-    let x = 5;
-    let y = x;
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
 
-    println!("x :  {}", x);
-    println!("y :  {}", y);
+    println!("s1 = {}, s2 = {}", s1, s2);
 }
