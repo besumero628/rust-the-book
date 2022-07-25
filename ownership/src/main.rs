3fda6813a4dcb824c141e8124f5b64f21eaa8110
besumero628 <besumero628@gmail.com>
2022-07-25 15:56:29 +0900

    参照した値変更ができないことの確認

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,12 +1,9 @@
 fn main() {
-    let s1 = String::from("hello");
-    let len = calculate_length(&s1);
-
-    println!("The length of '{}' is {}.", s1, len);
+    let s = String::from("hello");
     
 }
 
-fn calculate_length(s : &String) -> usize {
-    s.len()
+fn change(some_string : &String) {
+    some_string.push_str(",world")
 }
 
