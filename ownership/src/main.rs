e3143ba90cb4a54301ad0bf64215341419c4b268
besumero628 <besumero628@gmail.com>
2022-07-25 23:42:28 +0900

    文字列スライス

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,10 +1,23 @@
 fn main() {
-    let reference_to_nothing = dangle();
+    let mut s = String::from("hello world");
+    let word = first_word(&s);
+
+    println!("{}", word);
+
+    s.clear();
+
+    // println!("the first word is: {}", word);
     
 }
 
-fn dangle() -> &String {
-    let s = String::from("hello");
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
 
-    &s
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
