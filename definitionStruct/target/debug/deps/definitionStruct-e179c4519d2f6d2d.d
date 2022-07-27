e882bdab533928bf9ee1ec9e86aba8bf7f75dd0d
besumero628 <besumero628@gmail.com>
2022-07-27 23:41:28 +0900

    struct first commit

---
+++ definitionStruct/target/debug/deps/definitionStruct-e179c4519d2f6d2d.d
@@ -0,0 +1,5 @@
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/definitionStruct/target/debug/deps/definitionStruct-e179c4519d2f6d2d.rmeta: src/main.rs
+
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/definitionStruct/target/debug/deps/definitionStruct-e179c4519d2f6d2d.d: src/main.rs
+
+src/main.rs:
---
+++ definitionStruct/target/debug/deps/definitionStruct-ea37dbb561002ffa.d
@@ -0,0 +1,5 @@
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/definitionStruct/target/debug/deps/definitionStruct-ea37dbb561002ffa.rmeta: src/main.rs
+
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/definitionStruct/target/debug/deps/definitionStruct-ea37dbb561002ffa.d: src/main.rs
+
+src/main.rs:
