8010613cb21430bb10e1765ed929899a8378db3a
besumero628 <besumero628@gmail.com>
2022-07-28 10:05:26 +0900

    タプル構造体

--- definitionStruct/src/main.rs
+++ definitionStruct/src/main.rs
@@ -1,29 +1,7 @@
-struct User {
-    username: String,
-    email: String,
-    sign_in_count: u64,
-    active: bool,
-}
-
 fn main() {
+    struct Color(i32, i32, i32);
 
-    let mut user1 = User {
-        email: String::from("someone@example.com"),
-        username: String::from("someusername123"),
-        active: true,
-        sign_in_count: 1
-    };
-
-    user1.email = String::from("anotherone@example.com");
-    
+    let black = Color(0,0,0);
     println!("Hello, world!");
 }
 
-fn build_user(email: String, username: String) -> User {
-    User {
-        email,
-        username,
-        active: true,
-        sign_in_count: 1,
-    }
-}
