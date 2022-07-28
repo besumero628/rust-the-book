058dbf3fba7d1d07d865ba03428e58cb4c6658d0
besumero628 <besumero628@gmail.com>
2022-07-28 09:47:14 +0900

    struct作成関数

--- definitionStruct/src/main.rs
+++ definitionStruct/src/main.rs
@@ -1,17 +1,29 @@
-fn main() {
-    struct User {
-        username: String,
-        email: String,
-        sign_in_count: u64,
-        active: bool,
-    }
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
 
-    let user1 = User {
+fn main() {
+
+    let mut user1 = User {
         email: String::from("someone@example.com"),
         username: String::from("someusername123"),
         active: true,
         sign_in_count: 1
     };
+
+    user1.email = String::from("anotherone@example.com");
     
     println!("Hello, world!");
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
 }
