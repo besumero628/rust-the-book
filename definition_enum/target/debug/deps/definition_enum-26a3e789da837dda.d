db69800a8a4da15918e32ebaaaef009b7f25565a
besumero628 <besumero628@gmail.com>
2022-07-30 00:02:14 +0900

    enum first commit

---
+++ definition_enum/target/debug/deps/definition_enum-26a3e789da837dda.d
@@ -0,0 +1,5 @@
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/definition_enum/target/debug/deps/definition_enum-26a3e789da837dda.rmeta: src/main.rs
+
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/definition_enum/target/debug/deps/definition_enum-26a3e789da837dda.d: src/main.rs
+
+src/main.rs:
---
+++ definition_enum/target/debug/deps/definition_enum-5ce8ecde4b43a2cc.d
@@ -0,0 +1,5 @@
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/definition_enum/target/debug/deps/definition_enum-5ce8ecde4b43a2cc.rmeta: src/main.rs
+
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/definition_enum/target/debug/deps/definition_enum-5ce8ecde4b43a2cc.d: src/main.rs
+
+src/main.rs:
