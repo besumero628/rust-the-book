931596b44a0e0aa809dcda9943fbd2eb00a943ff
besumero628 <besumero628@gmail.com>
2022-07-30 11:15:28 +0900

    match文とOption

--- definition_enum/src/main.rs
+++ definition_enum/src/main.rs
@@ -1,24 +1,16 @@
 fn main() {
-    enum Coin {
-        Penny,
-        Nickel,
-        Dime,
-        Quarter,
-    }
-
-    fn value_incents(coin: Coin) -> u32 {
-        match coin {
-            Coin::Penny => {
-                println!("Lucky penny!");
-                1
-            },
-            Coin::Nickel => 5,
-            Coin::Dime => 10,
-            Coin::Quarter => 25,
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
         }
     }
 
-    let kind = Coin::Penny;
-    value_incents(kind);
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
 
+    println!("{:?}", six); //Some(6)
+    println!("{:?}", none); //None
 }
+
