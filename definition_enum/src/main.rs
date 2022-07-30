32c711e05f48c481cc40511c4f37f7268d3921d1
besumero628 <besumero628@gmail.com>
2022-07-30 09:19:41 +0900

    enum の設定

--- definition_enum/src/main.rs
+++ definition_enum/src/main.rs
@@ -1,8 +1,9 @@
 enum IpAddKind {
-    V4,
-    V6,
+    V4(String),
+    V6(String),
 }
 
 fn main() {
-    let four = IpAddKind::V4;
+    let four = IpAddKind::V4(String::from("127.0.0.1"));
+    let six = IpAddKind::V6(String::from("::1"));
 }
