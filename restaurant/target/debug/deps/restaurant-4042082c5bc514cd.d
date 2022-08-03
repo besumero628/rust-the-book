de8c7aa13c63ab4d6f8136c6548706d9581bd455
besumero628 <besumero628@gmail.com>
2022-08-03 09:55:06 +0900

    構造体の公開

---
+++ restaurant/target/debug/deps/restaurant-4042082c5bc514cd.d
@@ -0,0 +1,7 @@
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/restaurant/target/debug/deps/restaurant-4042082c5bc514cd.rmeta: src/lib.rs
+
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/restaurant/target/debug/deps/librestaurant-4042082c5bc514cd.rlib: src/lib.rs
+
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/restaurant/target/debug/deps/restaurant-4042082c5bc514cd.d: src/lib.rs
+
+src/lib.rs:
