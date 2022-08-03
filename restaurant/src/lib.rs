98bd223ea20391c4bf2a3d1c9e44c45a33215631
besumero628 <besumero628@gmail.com>
2022-08-03 22:20:49 +0900

    構造体のuse

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -11,3 +11,10 @@ pub fn eat_at_restaurant() {
     hosting::add_to_waitlist();
     hosting::add_to_waitlist();
 }
+
+use std::collections::HashMap;
+
+fn main() {
+    let mut map = HashMap::new();
+    map.insert(1, 2);
+}
