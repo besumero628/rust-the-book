33754825c2729bd8c604232518a80d7eba58b899
besumero628 <besumero628@gmail.com>
2022-08-03 22:59:37 +0900

    front_of_house の参照

--- restaurant/target/debug/deps/restaurant-8d5076eb1ae1a1fc.d
+++ restaurant/target/debug/deps/restaurant-8d5076eb1ae1a1fc.d
@@ -1,5 +1,6 @@
-/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/restaurant/target/debug/deps/restaurant-8d5076eb1ae1a1fc.rmeta: src/lib.rs
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/restaurant/target/debug/deps/restaurant-8d5076eb1ae1a1fc.rmeta: src/lib.rs src/front_of_house.rs
 
-/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/restaurant/target/debug/deps/restaurant-8d5076eb1ae1a1fc.d: src/lib.rs
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/restaurant/target/debug/deps/restaurant-8d5076eb1ae1a1fc.d: src/lib.rs src/front_of_house.rs
 
 src/lib.rs:
+src/front_of_house.rs:
