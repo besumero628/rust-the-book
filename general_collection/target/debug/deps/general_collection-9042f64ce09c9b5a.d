1f2949e1d33a0f84f9a96e2d2d7348b734e184da
besumero628 <besumero628@gmail.com>
2022-08-04 11:53:09 +0900

    first general collection

---
+++ general_collection/target/debug/deps/general_collection-9042f64ce09c9b5a.d
@@ -0,0 +1,5 @@
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/general_collection/target/debug/deps/general_collection-9042f64ce09c9b5a.rmeta: src/main.rs
+
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/general_collection/target/debug/deps/general_collection-9042f64ce09c9b5a.d: src/main.rs
+
+src/main.rs:
---
+++ general_collection/target/debug/deps/general_collection-a0094f3756a66e3d.d
@@ -0,0 +1,5 @@
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/general_collection/target/debug/deps/general_collection-a0094f3756a66e3d.rmeta: src/main.rs
+
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/general_collection/target/debug/deps/general_collection-a0094f3756a66e3d.d: src/main.rs
+
+src/main.rs:
