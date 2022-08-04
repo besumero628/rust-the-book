93098b8de49bc888de8b91a0281d85bebc12812c
besumero628 <besumero628@gmail.com>
2022-08-04 22:30:24 +0900

    enumを使用したvecの定義

--- general_collection/src/main.rs
+++ general_collection/src/main.rs
@@ -1,22 +1,14 @@
 fn main() {
-    let mut v = vec![1,2,3,4,5];
-
-    let first = &v[0];
-    println!("The first element is : {:?}", v);
-    println!("The first element is : {}", first);
-
-    v.push(6);
-
-    println!("The first element is : {:?}", v);
-
-    for i in &v {
-        println!("{}", i);
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
     }
 
-    for i in &mut v {
-        *i += 50;
-    }
-
-    println!("The first element is : {:?}", v);
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
  }
 
