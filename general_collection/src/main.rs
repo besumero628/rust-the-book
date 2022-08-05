e681a4abb6ed0169610224af2c8dc2882f9075a2
besumero628 <besumero628@gmail.com>
2022-08-06 00:11:34 +0900

    文字列の更新

--- general_collection/src/main.rs
+++ general_collection/src/main.rs
@@ -1,13 +1,26 @@
 fn main() {
-    let mut s1 = String::new();
+    let mut s = String::from("foo");
+    s.push_str("bar");
+    s.push('!');
 
-    let data = "initial contents";
-    let s2 = data.to_string();
+    let s1 = String::from("Hello ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2;
 
-    let s3 = String::from("initial contents");
+    let s4 = String::from("Goodbey");
+    let s5 = String::from("earth!");
+    let s6 = format!("{} - {}", s4, s5);
 
-    println!("{}", s1);
+    println!("{}", s);
+    // println!("{}", s1);
     println!("{}", s2);
     println!("{}", s3);
- }
+    println!("{}", s4);
+    println!("{}", s5);
+    println!("{}", s6);
 
+    let hello = "Здравствуйте";
+
+    let s7 = &hello[0..4];
+    println!("{}", s7);
+}
