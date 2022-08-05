3f57b942153fb83a2da2cb9ce926a9cbd0f86518
besumero628 <besumero628@gmail.com>
2022-08-05 23:54:33 +0900

    文字列の生成

--- general_collection/src/main.rs
+++ general_collection/src/main.rs
@@ -1,4 +1,13 @@
 fn main() {
-    let mut s = String::new();
+    let mut s1 = String::new();
+
+    let data = "initial contents";
+    let s2 = data.to_string();
+
+    let s3 = String::from("initial contents");
+
+    println!("{}", s1);
+    println!("{}", s2);
+    println!("{}", s3);
  }
 
