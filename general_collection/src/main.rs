4d57b388115e09330a9844bb35b748a43f4b4102
besumero628 <besumero628@gmail.com>
2022-08-06 10:42:30 +0900

    hashmapの所有権

--- general_collection/src/main.rs
+++ general_collection/src/main.rs
@@ -1,13 +1,14 @@
+use std::hash::Hash;
+
 fn main() {
     use std::collections::HashMap;
 
-    let mut scores = HashMap::new();
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
 
-    scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Yellow"), 50);
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
 
-    let teams = vec![String::from("Blue"), String::from("Yellow")];
-    let initial_scores = vec![10,50];
-
-    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+    println!("{:?}", map);
+    // println!("{:?}", field_name); この時点でmapに所有権がmoveしている
 }
