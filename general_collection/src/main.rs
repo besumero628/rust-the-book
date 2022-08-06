af07ad087beaee2f6ba923e3752b3a7eeb89808b
besumero628 <besumero628@gmail.com>
2022-08-06 11:13:48 +0900

    hashmapの更新

--- general_collection/src/main.rs
+++ general_collection/src/main.rs
@@ -4,16 +4,24 @@ fn main() {
     let mut scores = HashMap::new();
 
     scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Yellow"), 50);
+    scores.insert(String::from("Blue"), 25);
 
-    let team_name = String::from("Blue");
-    let score = scores.get(&team_name);
+    println!("{:?}", scores);
 
-    println!("{:?}", &score);
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
 
-    for (key, value) in &scores {
-        println!("{} {}", key, value);
+    println!("{:?}", scores);
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
     }
 
+    println!("{:?}", map);
 }
 
