8b4e786ce860c585cfd9b3cc5d3cc0ca0be95a2c
besumero628 <besumero628@gmail.com>
2022-08-12 12:34:41 +0900

    unwrap

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,20 +1,5 @@
-use std::{fs::File, io::ErrorKind};
+use std::{fs::File};
 
 fn main() {
-    let f = File::open("hello.txt");
-
-    let f = match f {
-        Ok(file) => file,
-        Err(ref error) if error.kind() == ErrorKind::NotFound => {
-            match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => {
-                    panic!("tried to create file but there was a problem: {:?}", e);
-                },
-            }
-        },
-        Err(error) => {
-            panic!("There was a problem opening the file: {:?}", error)
-        },
-    };
+    let f = File::open("hello.txt").unwrap();
 }
