ee6a4da77b4e352e0f161458ca6cf8c7096e4385
besumero628 <besumero628@gmail.com>
2022-08-12 12:53:12 +0900

    エラー委譲

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,5 +1,22 @@
-use std::{fs::File};
+use std::{io, io::Read, fs::File};
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
 
 fn main() {
-    let f = File::open("hello.txt").expect("Failed to open hello.txt");
+    let s = read_username_from_file().unwrap();
+    println!("{}", s);
 }
