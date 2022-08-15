88414e5456c327244c7b5d968da3f7002c419508
besumero628 <besumero628@gmail.com>
2022-08-15 11:55:38 +0900

    generic な引数と戻り値をもつ関数

--- definition_generics/src/main.rs
+++ definition_generics/src/main.rs
@@ -19,6 +19,17 @@ fn largest_char(list: &[char]) -> char {
     largest
 }
 
+fn largest<T>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
