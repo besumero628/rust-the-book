22e66a9f8cb9ee64e04003bc82ac0f8fd7c3788f
besumero628 <besumero628@gmail.com>
2022-08-15 12:10:11 +0900

    struct の generics

--- definition_generics/src/main.rs
+++ definition_generics/src/main.rs
@@ -19,15 +19,20 @@ fn largest_char(list: &[char]) -> char {
     largest
 }
 
-fn largest<T>(list: &[T]) -> T {
-    let mut largest = list[0];
+// fn largest<T>(list: &[T]) -> T {
+//     let mut largest = list[0];
 
-    for &item in list.iter() {
-        if item > largest {
-            largest = item;
-        }
-    }
-    largest
+//     for &item in list.iter() {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+//     largest
+// }
+
+struct Point<T> {
+    x: T,
+    y: T,
 }
 
 fn main() {
@@ -40,4 +45,8 @@ fn main() {
 
     let result = largest_char(&char_list);
     println!("The largest char is {}", result);
+
+    let integer = Point {x: 5, y: 10};
+    let integer = Point {x: 1.0, y: 4.0};
+    let integer = Point {x: "y", y: "x"};
 }
