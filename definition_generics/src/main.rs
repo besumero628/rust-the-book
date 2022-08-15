50ccf4d39da37d2c57bd09e744320e5bf125357a
besumero628 <besumero628@gmail.com>
2022-08-15 11:49:56 +0900

    数値と文字の最大を求める関数の作成

--- definition_generics/src/main.rs
+++ definition_generics/src/main.rs
@@ -1,13 +1,32 @@
+fn largest_i32(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item
+        }
+    }
+    largest
+}
+
+fn largest_char(list: &[char]) -> char {
+    let mut largest = list[0];
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let mut largest = number_list[0];
+    let result = largest_i32(&number_list);
+    println!("The largeset number is {}", result);
 
-    for number in number_list {
-        if number > largest {
-            largest = number;
-        }
-    }
+    let char_list = vec!['y', 'm', 'a', 'q'];
 
-    println!("The largeset number is {}", largest);
+    let result = largest_char(&char_list);
+    println!("The largest char is {}", result);
 }
