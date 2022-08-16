97a2cb340ad812d87b734f93ed02768bdee39e79
besumero628 <besumero628@gmail.com>
2022-08-16 10:49:18 +0900

    mixupの例

--- definition_generics/src/main.rs
+++ definition_generics/src/main.rs
@@ -1,67 +1,20 @@
-fn largest_i32(list: &[i32]) -> i32 {
-    let mut largest = list[0];
-
-    for &item in list.iter() {
-        if item > largest {
-            largest = item
-        }
-    }
-    largest
-}
-
-fn largest_char(list: &[char]) -> char {
-    let mut largest = list[0];
-    for &item in list.iter() {
-        if item > largest {
-            largest = item;
-        }
-    }
-    largest
-}
-
-// fn largest<T>(list: &[T]) -> T {
-//     let mut largest = list[0];
-
-//     for &item in list.iter() {
-//         if item > largest {
-//             largest = item;
-//         }
-//     }
-//     largest
-// }
-
-struct Point<T> {
+struct Point<T, U> {
     x: T,
-    y: T,
+    y: U,
 }
 
-impl<T> Point<T> {
-    fn x(&self) -> &T {
-        &self.x
+impl<T, U> Point<T, U> {
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point { x: self.x, y: other.y }
     }
 }
 
-impl Point<f32> {
-    fn distance_from_origin(&self) -> f32 {
-        (self.x.powi(2) + self.y.powi(2)).sqrt()
-    }
-}
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
+    let p1 = Point {x: 5, y: 10.4};
+    let p2 = Point{x: "Hello", y: "c"};
 
-    let result = largest_i32(&number_list);
-    println!("The largeset number is {}", result);
+    let p3 = p1.mixup(p2);
 
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest_char(&char_list);
-    println!("The largest char is {}", result);
-
-    let integer = Point {x: 5, y: 10};
-    let froat: Point<f32> = Point {x: 1.0, y: 4.0};
-    let chars = Point {x: "y", y: "x"};
-
-    println!("x = {}", integer.x());
-    println!("{}", froat.distance_from_origin());
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
