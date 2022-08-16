c72e474143825e8b484a6321a49600384d708adf
besumero628 <besumero628@gmail.com>
2022-08-16 23:29:41 +0900

    トレイト境界によるメソッド分岐

--- difinition_trait/src/main.rs
+++ difinition_trait/src/main.rs
@@ -1,23 +1,39 @@
-fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
-    let mut largest = list[0];
+use std::fmt::Display;
 
-    for &item in list.iter() {
-        if item > largest {
-            largest = item;
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self {x, y}
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
         }
     }
+}
 
-    largest
+impl Pair<i32> {
+    fn plus(&self) -> i32 {
+        self.x + self.y
+    }
 }
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
+    let p1 = Pair::new(5,6);
+    let p2 = Pair::new("y", "x");
 
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
+    p1.cmp_display();
+    p2.cmp_display();
 
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest(&char_list);
-    println!("The largest char is {}", result);
+    println!("{}", p1.plus())
 }
+
