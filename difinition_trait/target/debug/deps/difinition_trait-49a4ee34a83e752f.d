c6a67a5f14fa055ec04b47743a765343a924b709
besumero628 <besumero628@gmail.com>
2022-08-16 10:53:06 +0900

    first commit trait

---
+++ difinition_trait/target/debug/deps/difinition_trait-49a4ee34a83e752f.d
@@ -0,0 +1,5 @@
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/difinition_trait/target/debug/deps/difinition_trait-49a4ee34a83e752f.rmeta: src/main.rs
+
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/difinition_trait/target/debug/deps/difinition_trait-49a4ee34a83e752f.d: src/main.rs
+
+src/main.rs:
---
+++ difinition_trait/target/debug/deps/difinition_trait-cc9414a2261056ee.d
@@ -0,0 +1,5 @@
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/difinition_trait/target/debug/deps/difinition_trait-cc9414a2261056ee.rmeta: src/main.rs
+
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/difinition_trait/target/debug/deps/difinition_trait-cc9414a2261056ee.d: src/main.rs
+
+src/main.rs:
