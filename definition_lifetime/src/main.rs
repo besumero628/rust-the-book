748a2607291d5fd6d8754aa0bc86da3d7d83e296
besumero628 <besumero628@gmail.com>
2022-08-18 22:50:52 +0900

    構造体の中に参照がある場合

--- definition_lifetime/src/main.rs
+++ definition_lifetime/src/main.rs
@@ -1,18 +1,11 @@
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
-    if x.len() > y.len() {
-        x
-    } else {
-        y
-    }
+struct ImportantExcerpt<'a> {
+    part: &'a str,
 }
 
 fn main() {
-    let string1 = String::from("long string is long");
-    let result;
-    {
-        let string2 = String::from("xyz");
-        let result = longest(string1.as_str(), string2.as_str());
-        // 一番長い文字列は{}
-        println!("The longest string is {}", result);
-    }
+    let novel = String::from("call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
 }
