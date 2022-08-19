274b813e8de354a0d01e49150e96f86574a7ec2a
besumero628 <besumero628@gmail.com>
2022-08-19 22:09:02 +0900

    testの詳細を出力する

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,5 +1,6 @@
-pub fn add_two(a: i32) -> i32 {
-    a + 3
+pub fn greeting(name: &str) -> String {
+    // format!("Hello {}", name)
+    String::from("Hello!")
 }
 
 #[cfg(test)]
@@ -7,7 +8,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_adds_two() {
-        assert_eq!(4, add_two(2));
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was '{}'", result
+        );
     }
 }
