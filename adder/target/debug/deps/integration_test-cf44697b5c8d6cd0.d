ddf7034d877f07b4635f629367a921a649c8d548
besumero628 <besumero628@gmail.com>
2022-08-21 21:32:28 +0900

    tests dir の作成

---
+++ adder/target/debug/deps/integration_test-cf44697b5c8d6cd0.d
@@ -0,0 +1,5 @@
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/adder/target/debug/deps/integration_test-cf44697b5c8d6cd0.rmeta: tests/integration_test.rs
+
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/adder/target/debug/deps/integration_test-cf44697b5c8d6cd0.d: tests/integration_test.rs
+
+tests/integration_test.rs:
