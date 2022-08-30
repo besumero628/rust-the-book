e2928ef363739e6438421272d3b15d3be93c6a6e
besumero628 <besumero628@gmail.com>
2022-08-30 20:59:40 +0900

    cargo new addr

---
+++ add/target/debug/deps/adder-efdaed3695d251ee.d
@@ -0,0 +1,5 @@
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/add/target/debug/deps/adder-efdaed3695d251ee: adder/src/main.rs
+
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/add/target/debug/deps/adder-efdaed3695d251ee.d: adder/src/main.rs
+
+adder/src/main.rs:
