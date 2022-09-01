4e70e9f08a184df709d464dbc23244adfad6647e
besumero628 <besumero628@gmail.com>
2022-09-01 21:43:54 +0900

    参照はずし

--- definition_smartpointer/src/main.rs
+++ definition_smartpointer/src/main.rs
@@ -1,16 +1,7 @@
-use List::{Cons, Nil};
-
-#[derive(Debug)]
-enum List {
-    Cons(i32, Box<List>),
-    Nil,
-}
-
-
 fn main() {
-    let list = Cons(1,
-                            Box::new(Cons(2, 
-                                Box::new(Cons(3, 
-                                    Box::new(Nil))))));
-    println!("{:?}", list);
+    let x = 5;
+    let y = &x;
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
 }
