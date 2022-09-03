a6c9bf448ecd4fd9fcdefbce99a12bf8e6861faa
besumero628 <besumero628@gmail.com>
2022-09-03 19:14:18 +0900

    循環する参照

--- definition_smartpointer/src/main.rs
+++ definition_smartpointer/src/main.rs
@@ -17,5 +17,30 @@ impl List {
 }
 
 fn main () {
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
 
+    // aの最初の参照カウント = {}
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    // aの次の要素は = {:?}
+    println!("a next item = {:?}", a.tail());
+
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+
+    // b作成後のaの参照カウント = {}
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    // bの最初の参照カウント = {}
+    println!("b initial rc count = {}", Rc::strong_count(&b));
+    // bの次の要素 = {:?}
+    println!("b next item = {:?}", b.tail());
+
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
+    }
+
+    // aを変更後のbの参照カウント = {}
+    println!("b rc count after changing a = {}", Rc::strong_count(&b));
+    // aを変更後のaの参照カウント = {}
+    println!("a rc count after changing a = {}", Rc::strong_count(&a));
+
+    println!("a next item = {:?}", a.tail());  
 }
