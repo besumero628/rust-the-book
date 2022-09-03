49be791b714832bdef1e4233748012681fc01139
besumero628 <besumero628@gmail.com>
2022-09-03 19:24:41 +0900

    weak参照

--- definition_smartpointer/src/main.rs
+++ definition_smartpointer/src/main.rs
@@ -1,46 +1,28 @@
-use std::{cell::RefCell, rc::Rc};
-use List::{Cons, Nil};
+use std::{cell::RefCell, rc::{Rc, Weak}};
 
 #[derive(Debug)]
-enum List {
-    Cons(i32, RefCell<Rc<List>>),
-    Nil,
+struct Node {
+    value: i32,
+    parent: RefCell<Weak<Node>>,
+    children: RefCell<Vec<Rc<Node>>>,
 }
 
-impl List {
-    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
-        match *self {
-            Cons(_, ref item) => Some(item),
-            Nil => None,
-        }
-    }
-}
+fn main() {
+    let leaf = Rc::new(Node {
+        value: 3,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
+    });
 
-fn main () {
-    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
 
-    // aの最初の参照カウント = {}
-    println!("a initial rc count = {}", Rc::strong_count(&a));
-    // aの次の要素は = {:?}
-    println!("a next item = {:?}", a.tail());
+    let branch = Rc::new(Node {
+        value: 5,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![Rc::clone(&leaf)]),
+    });
 
-    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
 
-    // b作成後のaの参照カウント = {}
-    println!("a rc count after b creation = {}", Rc::strong_count(&a));
-    // bの最初の参照カウント = {}
-    println!("b initial rc count = {}", Rc::strong_count(&b));
-    // bの次の要素 = {:?}
-    println!("b next item = {:?}", b.tail());
-
-    if let Some(link) = a.tail() {
-        *link.borrow_mut() = Rc::clone(&b);
-    }
-
-    // aを変更後のbの参照カウント = {}
-    println!("b rc count after changing a = {}", Rc::strong_count(&b));
-    // aを変更後のaの参照カウント = {}
-    println!("a rc count after changing a = {}", Rc::strong_count(&a));
-
-    println!("a next item = {:?}", a.tail());  
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
 }
