0f214b314446971407a300289f6fd7b25c7f1e27
besumero628 <besumero628@gmail.com>
2022-09-07 13:57:33 +0900

    main

---
+++ definition_thread/target/debug/deps/definition_thread-8252421a739ebb2e.d
@@ -0,0 +1,5 @@
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/definition_thread/target/debug/deps/definition_thread-8252421a739ebb2e.rmeta: src/main.rs
+
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/definition_thread/target/debug/deps/definition_thread-8252421a739ebb2e.d: src/main.rs
+
+src/main.rs:
---
+++ definition_thread/target/debug/deps/definition_thread-cb321faf4d540b5d.d
@@ -0,0 +1,5 @@
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/definition_thread/target/debug/deps/definition_thread-cb321faf4d540b5d.rmeta: src/main.rs
+
+/Volumes/Extreme SSD/10_product/20_practice/80_Rust/01_thebook/definition_thread/target/debug/deps/definition_thread-cb321faf4d540b5d.d: src/main.rs
+
+src/main.rs:
