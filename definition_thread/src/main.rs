3c7fd3ffef470677c1001e8f6e016848c24de57c
besumero628 <besumero628@gmail.com>
2022-09-08 23:41:00 +0900

    複数送信機の設定

--- definition_thread/src/main.rs
+++ definition_thread/src/main.rs
@@ -3,6 +3,7 @@ use std::{sync::mpsc, thread, time::Duration};
 fn main () {
     let (tx, rx) = mpsc::channel();
 
+    let tx1 = mpsc::Sender::clone(&tx);
     thread::spawn(move || {
         let vals = vec![
             String::from("hi"),
@@ -12,9 +13,23 @@ fn main () {
         ];
 
         for val in vals {
-            tx.send(val).unwrap();
+            tx1.send(val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+        String::from("more"),
+        String::from("messages"),
+        String::from("for"),
+        String::from("you"),
+    ];
+
+    for val in vals {
+        tx.send(val).unwrap();
+        thread::sleep(Duration::from_secs(10));
+    }
     });
 
     for received in rx {
