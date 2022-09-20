9d36111303c35e2e26e67244ed513b7b86496dda
besumero628 <besumero628@gmail.com>
2022-09-20 17:57:36 +0900

    型エイリアス

--- advanced_features/src/main.rs
+++ advanced_features/src/main.rs
@@ -1,31 +1,8 @@
-use std::fmt::{self};
-
-trait OutlinePrint: fmt::Display {
-    fn outline_print(&self) {
-        let output = self.to_string();
-        let len = output.len();
-        println!("{}", "*".repeat(len + 4));
-        println!("*{}*", " ".repeat(len + 2));
-        println!("* {} *", output);
-        println!("*{}*", " ".repeat(len + 2));
-        println!("{}", "*".repeat(len + 4));
-    }
-}
-
-struct Point {
-    x: i32,
-    y: i32,
-}
-
-impl fmt::Display for Point {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "({}, {})", self.x, self.y)
-    }
-}
-
-impl OutlinePrint for Point{}
-
 fn main() {
-    let p = Point{x: 3, y: 4};
-    p.outline_print()
+    type Kilometers = i32;
+
+    let x: i32 = 5;
+    let y :Kilometers = 5;
+
+    println!("x + y = {}", x + y)
 }
