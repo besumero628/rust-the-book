d89cab504e80cf28a915f08b356003cbb2af855a
besumero628 <besumero628@gmail.com>
2022-09-20 11:21:24 +0900

    @束縛

--- definition_pattern_matching/src/main.rs
+++ definition_pattern_matching/src/main.rs
@@ -53,4 +53,25 @@ fn main() {
         Some(x) => println!("{}", x),
         None => (),
     }
+
+    enum Message {
+        Hello { id: i32 },
+    }
+    
+    let msg = Message::Hello { id: 5 };
+    
+    match msg {
+        Message::Hello { id: id_variable @ 3..=7 } => {
+            // 範囲内のidが見つかりました: {}
+            println!("Found an id in range: {}", id_variable)
+        },
+        Message::Hello { id: 10..=12 } => {
+            // 別の範囲内のidが見つかりました
+            println!("Found an id in another range")
+        },
+        Message::Hello { id } => {
+            // それ以外のidが見つかりました
+            println!("Found some other id: {}", id)
+        },
+    }
 }
