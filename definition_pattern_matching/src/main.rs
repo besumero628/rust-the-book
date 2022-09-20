008df5f9620d38899b96814cec89e94d92793579
besumero628 <besumero628@gmail.com>
2022-09-20 10:21:02 +0900

    名前付き変数マッチ

--- definition_pattern_matching/src/main.rs
+++ definition_pattern_matching/src/main.rs
@@ -7,4 +7,15 @@ fn main() {
         3 => println!("three"),
         _ => println!("anything"),
     }
+
+    let x2 = Some(5);
+    let y2 = 10;
+
+    match x2 {
+        Some(50) => println!("Got 50"),
+        Some(y2) => println!("Matched, y = {:?}", y2),
+        _ => println!("Default case, x2 = {:?}", x),
+    }
+
+    println!("at the end: x2 = {:?}, y2 = {:?}", x2, y2);
 }
